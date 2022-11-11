f62c8db94766a399038ce8daeec4f2e287a8eebd
melriosanch <melriosanch@gmail.com>
2022-11-11 14:28:35 -0600

    uchs cosas

--- js-07/assets/js/api.js
+++ js-07/assets/js/api.js
@@ -66,7 +66,7 @@ async function obtenerNombre (){
 
 }
 */
-const obtenerNombre = async () => {
+/*const obtenerNombre = async () => {
     try {
 
     let nombres = await nombre();
@@ -75,4 +75,55 @@ const obtenerNombre = async () => {
     alert(error)
 }
 }
-obtenerNombre()
+obtenerNombre()*/
+
+const cartaPokemon = document.getElementById('pokemon');
+const nombrePokemon = document.getElementById('nombre');
+const imagen = document.getElementById('img');
+const form = document.getElementById('form')
+
+
+
+const pokedex = async (numero) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${numero}`;
+    try {
+
+        const respuesta = await fetch(url);
+        const pokemon = await respuesta.json();
+        
+        console.log(pokemon.name);
+
+        const datos = {
+            nombre: pokemon.name,
+            imagen: pokemon.sprites.other['official-artwork'].front_default,
+        }
+
+        console.log(datos.imagen);
+
+        nombrePokemon.textContent = datos.nombre;
+        imagen.innerHTML = `
+            <img src="${datos.imagen}" alt="imagen de ${datos.nombre}" width="250"/>
+        `;
+        
+        
+    } catch (error) {
+
+
+        
+    }
+}
+
+pokedex();
+
+form.addEventListener('submit', (evento)=>{
+    //codigo
+    evento.preventDefault();
+    const valor = document.getElementById('idP').value;
+    console.log(valor)
+    pokedex(valor);
+   // console.log(evento.target)
+   form.reset()
+
+});
+
+
